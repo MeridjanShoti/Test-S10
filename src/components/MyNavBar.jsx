3af0a734775999d54b32feb302790cea2a2e82aa
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-14 23:12:49 +0100

    a

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -1,8 +1,10 @@
 import { Col, Container, Form, Nav, Navbar, Row } from "react-bootstrap";
-import { NavLink, useLocation } from "react-router";
+import { NavLink, useLocation, useNavigate } from "react-router";
 import logo from "../assets/Weather.png";
 const MyNavBar = () => {
+  const navigate = useNavigate();
   const location = useLocation();
+
   return (
     <Navbar expand="lg" className="bg-body-transparent">
       <Container>
@@ -11,32 +13,36 @@ const MyNavBar = () => {
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <div className="d-flex flex-row justify-content-between flex-wrap mt-3">
-            <Nav className="me-auto gap-3 flex-row">
-              <NavLink
-                className={`flex-row text-white text-decoration-none ${
-                  location.pathname === "/" ? "nav-link nav-link-active fw-bold" : "nav-link"
-                }`}
-                to="/"
-              >
-                Home
-              </NavLink>
-              <NavLink
-                className={`text-white text-decoration-none ${
-                  location.pathname === "/bucosenzafondo" ? "nav-link nav-link-active fw-bold" : "nav-link"
-                }`}
-                to="/bucosenzafondo"
-              >
-                Non cliccare qui
-              </NavLink>
-            </Nav>
-            <Form>
-              <Row>
-                <Col xs="auto">
-                  <Form.Control type="text" placeholder="Cerca città" className=" mr-sm-2" />
-                </Col>
-              </Row>
-            </Form>
+          <div className="d-flex flex-row justify-content-between flex-wrap mt-3 w-100">
+            <div>
+              <Nav className="me-auto gap-3 flex-row">
+                <NavLink
+                  className={`flex-row text-white text-decoration-none ${
+                    location.pathname === "/" ? "nav-link nav-link-active fw-bold" : "nav-link"
+                  }`}
+                  to="/"
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  className={`text-white text-decoration-none ${
+                    location.pathname === "/bucosenzafondo" ? "nav-link nav-link-active fw-bold" : "nav-link"
+                  }`}
+                  to="/bucosenzafondo"
+                >
+                  Non cliccare qui
+                </NavLink>
+              </Nav>
+            </div>
+            <div>
+              <Form>
+                <Row>
+                  <Col xs="auto">
+                    <Form.Control type="text" placeholder="Cerca città" className=" mr-sm-2" />
+                  </Col>
+                </Row>
+              </Form>
+            </div>
           </div>
         </Navbar.Collapse>
       </Container>
