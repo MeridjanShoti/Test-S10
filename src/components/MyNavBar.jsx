dcf8be2f2109627f97f8223b76b9b80f6cfd7eca
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-14 23:07:05 +0100

    a

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -9,33 +9,35 @@ const MyNavBar = () => {
         <NavLink className="text-decoration-none me-5" to="/">
           <img src={logo} width="30px"></img>
         </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav border-1" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto gap-3">
-            <NavLink
-              className={`text-white text-decoration-none ${
-                location.pathname === "/" ? "nav-link nav-link-active fw-bold" : "nav-link"
-              }`}
-              to="/"
-            >
-              Home
-            </NavLink>
-            <NavLink
-              className={`text-white text-decoration-none ${
-                location.pathname === "/bucosenzafondo" ? "nav-link nav-link-active fw-bold" : "nav-link"
-              }`}
-              to="/bucosenzafondo"
-            >
-              Non cliccare qui
-            </NavLink>
-          </Nav>
-          <Form>
-            <Row>
-              <Col xs="auto">
-                <Form.Control type="text" placeholder="Cerca città" className=" mr-sm-2" />
-              </Col>
-            </Row>
-          </Form>
+          <div className="d-flex flex-row justify-content-between flex-wrap mt-3">
+            <Nav className="me-auto gap-3 flex-row">
+              <NavLink
+                className={`flex-row text-white text-decoration-none ${
+                  location.pathname === "/" ? "nav-link nav-link-active fw-bold" : "nav-link"
+                }`}
+                to="/"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className={`text-white text-decoration-none ${
+                  location.pathname === "/bucosenzafondo" ? "nav-link nav-link-active fw-bold" : "nav-link"
+                }`}
+                to="/bucosenzafondo"
+              >
+                Non cliccare qui
+              </NavLink>
+            </Nav>
+            <Form>
+              <Row>
+                <Col xs="auto">
+                  <Form.Control type="text" placeholder="Cerca città" className=" mr-sm-2" />
+                </Col>
+              </Row>
+            </Form>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
